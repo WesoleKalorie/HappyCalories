294bb650ce3a56f01a2d2e493bdea9cd7dfaa8b4
Emperor1996 <kamil.szyda1996@gmail.com>
2018-05-28 10:52:13 +0200

    fsafaw

--- HappyCalories/LoginView.cs
+++ HappyCalories/LoginView.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace HappyCalories
 {
@@ -15,6 +17,28 @@ namespace HappyCalories
         public LoginView()
         {
             InitializeComponent();
+        }
+
+        private void LoginView_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection polaczenie = new SqlConnection(@"Data source=ec2-54-75-227-92.eu-west-1.compute.amazonaws.com;
+                                                             database=d3smaqev4brg5l;
+                                                             User id=dabgiwdzivsnni;
+                                                             Password=690ec2917fa7a255c72b411966690ce5836b9e35c41469e64e126cd63a1eeaee;");
+                polaczenie.Open();
+
+               
+            }
+            catch
+            {
+                string message = "nie udało sie połączyć";
+                string caption = "Error";
+                var result = MessageBox.Show(message,caption, MessageBoxButtons.OK);
+                
+                
+            }
         }
 
         // znowu sprawdzam, czy jakoś to działa :)
--- HappyCalories/Model.cs
+++ HappyCalories/Model.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace HappyCalories
 {
     class Model
     {
-        //sagsagsagsa
+        bool sqlConnection()
+        {
+            try
+            {
+                SqlConnection polaczenie = new SqlConnection(@"Data source=SERWER\SQLEXPRESS;
+                                                             database=adresy;
+                                                             User id=sa;
+                                                             Password=haslo;");
+                polaczenie.Open();
+
+                return true;
+
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
 
     }
 }
